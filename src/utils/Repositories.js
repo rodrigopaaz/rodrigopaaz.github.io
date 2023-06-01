0003b340c06bf87d1ad7750c4145d4987c59595f
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-06-01 16:21:37 -0300

    fead: add project blue store to repositories

--- src/utils/Repositories.js
+++ src/utils/Repositories.js
@@ -2,27 +2,82 @@ import walletImg from '../images/trybe-wallet-img.png'
 import tunesImg from '../images/trybe-tunes-img.png'
 import tryunfoImg from '../images/tryunfo-img.png'
 import recipesImg from '../images/recipes-app-img.png'
+import githubImg from '../images/github-img.jpeg'
+import tfcImg from '../images/tfc.png'
+import blueStore from '../images/bluestore.png'
 
 const objectRepo = [{
         url: 'https://rodrigopaaz.github.io/project-trybe-wallet/',
+        github: 'https://github.com/rodrigopaaz/project-trybe-wallet',
         image: walletImg,
         descricao: 'Trybe Wallet',
+        modulo: 'frontend',
+        stacks: []
+    },
+    {
+        url: 'https://blue-store-project-as3i.vercel.app/main',
+        github: 'https://github.com/rodrigopaaz/blue-store-project',
+        image: blueStore,
+        descricao: 'Blue Store',
+        modulo: 'fullstack',
+        stacks: [],
+        destaque: true,
     },
     {
         url: 'https://rodrigopaaz.github.io/project-trybe-tunes/',
+        github: 'https://github.com/rodrigopaaz/project-trybe-tunes',
         image: tunesImg,
         descricao: 'Trybe Tunes',
+        modulo: 'frontend',
+        stacks: []
     },
     {
         url: 'https://rodrigopaaz.github.io/project-tryunfo/',
+        github: 'https://github.com/rodrigopaaz/project-tryunfo',
         image: tryunfoImg,
         descricao: 'Tryunfo',
+        modulo: 'frontend',
+        stacks: []
     },
     {
         url: 'https://rodrigopaaz.github.io/project-recipes-app/',
+        github: 'https://github.com/rodrigopaaz/project-recipes-app',
         image: recipesImg,
         descricao: 'Recipes App',
-    }
-    ]
+        modulo: 'frontend',
+        stacks: []
+    },
+        {
+            url: '',
+            github: 'https://github.com/rodrigopaaz/PROJECT-TRYBESMITH',
+            image: githubImg,
+            descricao: 'Trybe Smith',
+            modulo: 'backend',
+            stacks: []
+        },
+        {
+        url: '',
+        github: 'https://github.com/rodrigopaaz/PROJECT-TRYBERS-AND-DRAGONS',
+        image: githubImg,
+        descricao: 'Trybers and Dragons',
+        modulo: 'backend',
+        stacks: []
+    },
+        {
+            url: '',
+            github: 'https://github.com/rodrigopaaz/PROJECT-BLOGS-API',
+            image: githubImg,
+            descricao: 'Blogs API',
+            modulo: 'backend',
+            stacks: []
+        },
+        {
+        url: 'https://project-tfc-production.up.railway.app/leaderboard',
+        github: 'https://github.com/rodrigopaaz/PROJECT-TFC',
+        image: tfcImg,
+        descricao: 'TFC',
+        modulo: 'fullstack',
+        stacks: []
+    },]
   export default objectRepo;
 
